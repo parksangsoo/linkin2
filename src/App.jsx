f8e056eaea4d80db0f7d1f5f26c04e704b759ed7
parksangsoo <pssw0728@gmail.com>
2021-03-18 18:45:47 +0900

    링크드인 헤더부분 초기작업

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './header/header';
+
+function App() {
+  return (
+    <div className="App">
+      {/*header*/}
+      <Header/>
+      {/*left*/}
+      {/*center*/}
+      {/*right*/}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/header/header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+    return(
+        <div className="header">
+            <div className="headerleft">
+                <img className="linkinicon" src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg" alt=""/>
+                <div className="sreach">
+
+                    <input type="text"/>
+                </div>
+                
+            </div>
+            <div className="headerright">
+
+            </div>
+        </div>
+    );
+};
+
+export default Header;
