a203ab02fd4b7d1ded7deb763012d33b3c3f5160
parksangsoo <pssw0728@gmail.com>
2021-03-24 14:01:11 +0900

    feed 태그입력 작업

--- src/feed/Feed.jsx
+++ src/feed/Feed.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './Feed.css';
-
+import CreateIcon from '@material-ui/icons/Create';
 
 const Feed = () => {
     return(
         <div className="feed">
-            feed
+            <div className="feedinput">
+                <div className="inputbox">
+                    <CreateIcon />
+                    <input type="text"/>
+                </div>
+                <div className="inputoption">
+
+                </div>
+            </div>
+
+            <div className="feedlist">
+
+            </div>
         </div>
         
     );
---
+++ src/feed/Feedoption.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Feedoption.css';
+
+
+const FeedOption = () => {
+    return(
+        <div className="feedoption">
+            widgets
+        </div>
+        
+    );
+};
+
+export default FeedOption;
