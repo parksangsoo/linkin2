00903e2f1db32b5757c4aba426de1bf7c04b2c7b
parksangsoo <pssw0728@gmail.com>
2021-03-24 13:50:10 +0900

    sidebar css 작업

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -3,9 +3,41 @@ import './Sidebar.css';
 
 
 const SideBar = () => {
+
+    const recentItem = (topic) => (
+        <div className="recentItem">
+            <span className="hashtag">#</span>
+            <p>{topic}</p>
+        </div>
+    )
+
     return(
         <div className="sidebar">
-            sidebar
+            <div className="sidebarup">
+                <img src="https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHw%3D&w=1000&q=80" alt=""/>
+                <h4>박상수</h4>
+                <h4>asdf@gmail.com</h4>
+            </div>
+
+            <div className="sidebarmid">
+                    <div className="option1">
+                        <p>Who viewed you</p>
+                        <p className="statnumber">2,543</p>
+                    </div>
+                    <div className="option1">
+                        <p>Views on post</p>
+                        <p className="statnumber">2,448</p>
+                    </div>
+            </div>
+
+            <div className="sidebardown">
+                <p>Recnet</p>
+                {recentItem('reactjs')}
+                {recentItem('programming')}
+                {recentItem('softwareengineering')}
+                {recentItem('design')}
+                {recentItem('developer')}
+            </div>
         </div>
         
     );
